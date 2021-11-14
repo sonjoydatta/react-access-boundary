4148aa2d8a3d1f3ee74c1d0d5134c19fcd34bcc9
Sonjoy Datta <sonjoy.dat@gmail.com>
2021-11-15 00:16:20 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# React Access Boundary
---
+++ src/libs/AccessFallback.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const AccessFallback = () => {
+	return (
+		<div>
+			<p>You are not authorised to access this page</p>
+		</div>
+	);
+};
